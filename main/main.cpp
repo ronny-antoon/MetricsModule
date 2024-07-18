e44b62ccd0d60b1ed70a03f3ce4c50ec6d2ffdeb
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2024-07-18 09:31:15 +0300

    .

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,20 @@
-#include "MetricsModule.test.hpp"
+#include "MetricsModule.hpp"
+#include "wifiHelper.hpp"
 
 extern "C" void app_main()
 {
-    UNITY_BEGIN();
-    unity_run_all_tests();
-    UNITY_END();
+    bool * wifiConnected = new bool(false);
+    wifi_init_sta(wifiConnected);
+    while (!*wifiConnected)
+    {
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    MetricsModule * metricsModule = new MetricsModule(nullptr, "TestLocation", nullptr);
+    metricsModule->start();
+
+    vTaskDelay(20000 / portTICK_PERIOD_MS);
+
+    delete metricsModule;
 }
